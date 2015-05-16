6340738ace9418d44cc237469226e0eb7c0adea2
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-05-16 15:42:46 +0300

    Add AttackAction::activate

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -2,8 +2,20 @@ require "spec_helper"
 require_relative '../../lib/attack_action'
 
 describe AttackAction do
-  let(:hero) { double('hero') }
-  let(:action) { AttackAction.new hero }
+
+  let(:hero) { double("hero", strength: 3) }
+  let(:action) { AttackAction.new hero, dicepool  }
+  let(:dicepool) { double("dicepool") }
+
+
+  describe 'activate' do
+    let(:monster) { double('monster', toughness: 2) }
+
+    it "makes strength check against target toughness" do
+      expect(dicepool).to receive(:skill_check).with(hero.strength, monster.toughness)
+      action.activate(monster)
+    end
+  end
   
   it "responds to activate message" do
     expect(action).to respond_to(:activate)
