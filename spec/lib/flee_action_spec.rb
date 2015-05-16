e30decc1d4a2afecf635cf25883c44f29153b18a
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-05-16 17:50:50 +0300

    FleeAction effects implemented

--- spec/lib/flee_action_spec.rb
+++ spec/lib/flee_action_spec.rb
@@ -1,14 +1,31 @@
 require "spec_helper"
 require_relative '../../lib/flee_action'
 
+
 describe FleeAction do
 
-  let(:hero) { double("hero", strength: 3, stealth: 3 ) }
+  let(:hero) { double("hero", strength: 3, stealth: 3, flee: nil, damage: nil) }
   let(:action) { FleeAction.new hero, dicepool  }
   let(:dicepool) { double("dicepool") }
   let(:monster) { double('monster', notice: 2, damage: 4, toughness: 2, kill: nil ) }
 
   describe 'effect' do
+
+    context "success" do
+      it "sends flee message to the owner" do
+        allow(dicepool).to receive(:skill_check).and_return(true)
+        expect(hero).to receive(:flee)
+        action.activate(monster)
+      end
+    end
+
+    context "failure" do
+      it "deals damage to the owner" do
+        allow(dicepool).to receive(:skill_check).and_return(false)
+        expect(hero).to receive(:damage).with(monster.damage)
+        action.activate(monster)
+      end
+    end
   end
 
 
