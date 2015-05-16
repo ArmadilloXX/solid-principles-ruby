336de377c2e6058f713e8ac5657d844ab5410f44
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-05-16 16:21:50 +0300

    Add new methods to Hero

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,10 +2,9 @@ require "spec_helper"
 require_relative '../../lib/hero'
 
 describe Hero do
+  let(:hero) { Hero.new }
 
-  context "default attributes" do
-    let(:hero) { Hero.new }
-
+  describe "default attributes" do
     it "has default strength of 3" do
       expect(hero.strength).to eq(3)
     end
@@ -14,9 +13,34 @@ describe Hero do
       expect(hero.health).to eq(10)
     end
 
+    it "has default gold of 0" do
+      expect(hero.gold).to eq(0)
+    end
+
+    it "has default exp of 0" do
+      expect(hero.exp).to eq(0)
+    end
   end
 
-  context "custom attributes" do
+  it "can be damaged" do
+    hero.damage(3)
+    hero.damage(2)
+    expect(hero.health).to eq(5)
+  end
+
+  it "gains gold" do
+    hero.gain_gold(10)
+    hero.gain_gold(5)
+    expect(hero.gold).to eq(15)
+  end
+
+  it "gains_exp" do
+    hero.gain_exp(5)
+    hero.gain_exp(7)
+    expect(hero.exp).to eq(12)
+  end
+
+  describe "custom attributes" do
     let(:hero) { Hero.new strength: 3, health: 15 }
 
     it "can be initialized with custom strength" do
